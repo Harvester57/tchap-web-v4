c695d34b470c32a365d25750b415ba38cd9444e2
marc.sirisak <marc.sirisak@beta.gouv.fr>
2025-02-06 16:02:30 +0100

    TCHAP: change faq url to numerique.gouv.fr

--- src/tchap/util/TchapUrls.ts
+++ src/tchap/util/TchapUrls.ts
@@ -6,25 +6,28 @@ export default class TchapUrls {
     public static secureBackupFragment =  "sauvegarde-automatique";
 
     //tchap status page
-    public static statusPage =  "https://status.tchap.numerique.gouv.fr"
+    public static statusPage =  "https://status.tchap.numerique.gouv.fr";
+
+    // tchap help base url
+    public static helpBaseUrl = "https://aide.tchap.numerique.gouv.fr"
 
     //tchap FAQ page on locked messages.
-    public static lockedMessagesPage = "https://aide.tchap.beta.gouv.fr/fr/article/dechiffrement-en-cours-mes-messages-restent-verrouilles-atnp15/";
+    public static lockedMessagesPage = `${TchapUrls.helpBaseUrl}/fr/article/dechiffrement-en-cours-mes-messages-restent-verrouilles-atnp15/`;
 
     // help page to verify decices, used 
-    public static helpVerifyDevicesPage = "https://aide.tchap.beta.gouv.fr/fr/article/comment-verifier-un-nouvel-appareil-sur-tchap-xm0b0y/";
+    public static helpVerifyDevicesPage = `${TchapUrls.helpBaseUrl}/fr/article/comment-verifier-un-nouvel-appareil-sur-tchap-xm0b0y/`;
 
     // help page for notification by email
-    public static helpEmailNotification = "https://aide.tchap.beta.gouv.fr/fr/article/notification-par-email-draft-6k7k89/";
+    public static helpEmailNotification = `${TchapUrls.helpBaseUrl}/fr/article/notification-par-email-draft-6k7k89/`;
 
     // help create space 
-    public static helpCreateSpace = "https://aide.tchap.beta.gouv.fr/fr/article/comment-creer-un-espace-sur-tchap-1wmlenx";
+    public static helpCreateSpace = `${TchapUrls.helpBaseUrl}/fr/article/comment-creer-un-espace-sur-tchap-1wmlenx`;
 
     // help user onboarding
-    public static helpUserOnboarding = "https://aide.tchap.beta.gouv.fr/fr/article/guide-de-prise-en-main-de-tchap-oswyn1/";
+    public static helpUserOnboarding = `${TchapUrls.helpBaseUrl}/fr/article/guide-de-prise-en-main-de-tchap-oswyn1/`;
 
     // help manually import tchap keys
-    public static helpManuallyImportKeys= "https://aide.tchap.beta.gouv.fr/fr/article/comment-importer-manuellement-mes-cles-tchap-cles-de-chiffrement-web-xh376r"
+    public static helpManuallyImportKeys= `${TchapUrls.helpBaseUrl}/fr/article/comment-importer-manuellement-mes-cles-tchap-cles-de-chiffrement-web-xh376r`;
 
-    public static helpProconnectInstances = "https://aide.tchap.beta.gouv.fr/fr/article/se-connecter-a-tchap-avec-proconnect-1dh1peg";
+    public static helpProconnectInstances = `${TchapUrls.helpBaseUrl}/fr/article/se-connecter-a-tchap-avec-proconnect-1dh1peg`;
 }
